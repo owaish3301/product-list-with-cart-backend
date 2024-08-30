9092edad37d91092ac3906077c7435efcf066881
owaish3301 <owaish3301@gmail.com>
2024-08-30 11:54:09 +0530

    added cross origin headers

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/api', (req, res) => {
   fs.readFile(path.join(__dirname, 'public', 'data.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error("Got an error while reading the file: ", err);
-      res.status(500).send("Error reading the json file");
-    } else if (data) {
-      try {
-        res.status(200).json(JSON.parse(data));
-      } catch {
-        res.status(500).send("Error parsing the json data");
+      if (err) {
+          console.error("Got an error while reading the file: ", err);
+          res.status(500).json({ error: "Error reading the json file" });
+      } else {
+          try {
+              const jsonData = JSON.parse(data);
+              
+              res.json(jsonData);
+          } catch (parseError) {
+              console.error("Error parsing the json data:", parseError);
+              res.status(500).json({ error: "Error parsing the json data" });
+          }
       }
-    }
   });
 });
 
